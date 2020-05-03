3dc967921253e0a2841744d6b472f01d90aa49a6
Hassan Ishmam <hassan.ishmam@gmail.com>
2020-05-02 22:11:31 -0700

    changed main

--- src/p2p_project/P2P_Project.java
+++ src/p2p_project/P2P_Project.java
@@ -6,11 +6,12 @@ public class P2P_Project {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Enter a number: ");
-		Scanner s = new Scanner(System.in);
 		
-		int x = s.nextInt();
-		System.out.print(x);
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("1. Run Client\n2. Run Server\n\nEnter your choice:");
+		int userChoice = scan.nextInt();
+		System.out.print(userChoice);
 
 		
 		
