930c0bddeabe3561fb06adf3259d0fe3a658ab7f
Hassan Ishmam <hassan.ishmam@gmail.com>
2020-05-02 17:10:57 -0700

    Initial commit

---
+++ src/p2p_project/P2P_Project.java
@@ -0,0 +1,21 @@
+package p2p_project;
+
+import java.util.Scanner;
+
+public class P2P_Project {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Enter a number: ");
+		Scanner s = new Scanner(System.in);
+		
+		int x = s.nextInt();
+		System.out.print(x);
+
+		
+		
+		
+		
+	}
+
+}
