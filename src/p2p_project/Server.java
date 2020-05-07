aa0bf020fcb84673086da8e76d61087ff7ea0508
Hassan Ishmam <hassan.ishmam@gmail.com>
2020-05-07 01:52:10 -0700

    Refactoring code

--- src/p2p_project/Server.java
+++ src/p2p_project/Server.java
@@ -17,10 +17,37 @@ public class Server {
     public Server(int port) throws ClassNotFoundException 
     { 
     	
+    	try {
+    		server = new ServerSocket(port);
+			System.out.println("Server started!! ");
+			System.out.println(" ");
+			System.out.println("Waiting for the Client to be connected ..");
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+    	
+    	while(true)
+    	{
+    		try{
+    			
+    			socket = server.accept();
+    				
+    		}
+    		catch(IOException e)
+    		{
+    			System.out.println("I/O error: " +e);
+    		}
+    		
+    		//new ClientHandler(Socket socket).start();
+    		//new ServerTestClass(socket,globalArray).start();
+    	}
+    	
+    	/*Works with multiple files v3.0
     	 try
          { 
-    		 
-             
+    		              
              server = new ServerSocket(port); 
              System.out.println("Server started"); 
    
@@ -48,12 +75,12 @@ public class Server {
             	 dos.writeUTF(name);
             	 
             	 FileInputStream fis = new FileInputStream(file);
-            	    BufferedInputStream bis = new BufferedInputStream(fis);
+            	 BufferedInputStream bis = new BufferedInputStream(fis);
 
-            	    int theByte = 0;
-            	    while((theByte = bis.read()) != -1) bos.write(theByte);
+            	 int theByte = 0;
+            	 while((theByte = bis.read()) != -1) bos.write(theByte);
 
-            	    bis.close();
+            	 bis.close();
              }
              //oos.writeObject(fileNames); 
              
@@ -69,101 +96,10 @@ public class Server {
          { 
              System.out.println(i); 
          }
-    	 
+    	 */
+
     	
-        
-    	//Transfers only specified file types	v1.0
     	/*
-        try
-        { 
-        	
-            server = new ServerSocket(port); 
-            System.out.println("Server started"); 
-  
-            System.out.println("Waiting for a client ..."); 
-  
-            socket = server.accept(); 
-            System.out.println("Client accepted");
-            
-            
-            File [] listOfFiles = getListOfFiles(directory);
-            int numberOfFiles = listOfFiles.length;
-            
-            FileInputStream fin;
-            OutputStream os;
-            
-            DataOutputStream dos = (DataOutputStream) socket.getOutputStream();
-            dos.write(numberOfFiles);
-            
-            os = socket.getOutputStream();
-            os.flush();
-            
-            for(int i = 0; i<listOfFiles.length; i++) {
-            	
-            	System.out.println("Getting file " + listOfFiles[i].getName());
-            	
-            	fin = new FileInputStream(directory + listOfFiles[i].getName());
-            	
-            	System.out.println("Converting to byte file " + listOfFiles[i].getName());
-                
-                byte byteArr[] = new byte[2000];
-                fin.read(byteArr, 0, byteArr.length);
-                
-                System.out.println("Sending file " + listOfFiles[i].getName());
-                
-                
-                os.write(byteArr, 0, byteArr.length);
-                os.flush();
-                
-                System.out.println("Sent file " + listOfFiles[i].getName());
-                
-                fin.close();
-                
-            	
-            }
-            
-            os.close();
-            
-            System.out.println("Closing Connection..");
-            
-            socket.close();
-           
-            
-        }
-        
-        catch(IOException i) 
-        { 
-            System.out.println(i); 
-        }
-        */
-    	
-    	//v2.0
-    	/*
-    	try {
-    		server = new ServerSocket(port);
-			System.out.println("Server started!! ");
-			System.out.println(" ");
-			System.out.println("Waiting for the Client to be connected ..");
-		}
-		catch(IOException e)
-		{
-			e.printStackTrace();
-		}
-    	
-    	while(true)
-    	{
-    		try{
-    				socket = server.accept();
-    				
-    		}
-    		catch(IOException e)
-    		{
-    			System.out.println("I/O error: " +e);
-    		}
-    		
-    		new ClientHandler(Socket socket).start();
-    		//new ServerTestClass(socket,globalArray).start();
-    	}
     	
          
         
@@ -173,10 +109,7 @@ public class Server {
         
         
        
-    }
-    
-
-    
+    } 
     
 }
 /*
